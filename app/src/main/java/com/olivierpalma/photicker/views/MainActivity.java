d68160b51d7c8d8e6c370cfaa1ca8a7fa843352f
Olivier Palma <olivierpalmasouza@gmail.com>
2017-12-28 14:34:08 -0200

    Implemantando a listagem dos stickers

--- app/src/main/java/com/olivierpalma/photicker/views/MainActivity.java
+++ app/src/main/java/com/olivierpalma/photicker/views/MainActivity.java
@@ -2,8 +2,13 @@ package com.olivierpalma.photicker.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.olivierpalma.photicker.R;
+import com.olivierpalma.photicker.utils.ImageUtil;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        List<Integer> mListImages = ImageUtil.getImageList();
+
+        final LinearLayout content = (LinearLayout) this.findViewById(R.id.horizontal_content);
+
+        for (Integer imageId : mListImages) {
+            ImageView image = new ImageView(this);
+            image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
+            image.setPadding(20, 10, 20, 10);
+            content.addView(image);
+        }
 
     }
 }
