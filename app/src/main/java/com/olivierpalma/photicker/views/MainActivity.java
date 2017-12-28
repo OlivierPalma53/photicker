6f4f007e0d311f282ca2ea75e89dc58786952c2b
Olivier Palma <olivierpalmasouza@gmail.com>
2017-12-28 15:46:18 -0200

    Implementando clique nos stickers

--- app/src/main/java/com/olivierpalma/photicker/views/MainActivity.java
+++ app/src/main/java/com/olivierpalma/photicker/views/MainActivity.java
@@ -1,9 +1,12 @@
 package com.olivierpalma.photicker.views;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.olivierpalma.photicker.R;
 import com.olivierpalma.photicker.utils.ImageUtil;
@@ -23,14 +26,41 @@ public class MainActivity extends AppCompatActivity {
 
         List<Integer> mListImages = ImageUtil.getImageList();
 
+        final RelativeLayout  relativeLayout = (RelativeLayout) this.findViewById(R.id.relative_photo_content_draw);
         final LinearLayout content = (LinearLayout) this.findViewById(R.id.horizontal_content);
 
         for (Integer imageId : mListImages) {
             ImageView image = new ImageView(this);
             image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
             image.setPadding(20, 10, 20, 10);
+
+            BitmapFactory.Options dimensions = new BitmapFactory.Options();
+            dimensions.inJustDecodeBounds = true;
+
+            BitmapFactory.decodeResource(getResources(), imageId, dimensions);
+
+            final int width = dimensions.outWidth;
+            final int height = dimensions.outHeight;
+
+            image.setOnClickListener(onClickImageOption(relativeLayout, imageId, width, height));
+
             content.addView(image);
         }
 
+    }
+
+    private View.OnClickListener onClickImageOption(final RelativeLayout relativeLayout, final Integer imageId, int width, int height) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ImageView image = new ImageView(MainActivity.this);
+                image.setImageResource(imageId);
+                relativeLayout.addView(image);
+
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) image.getLayoutParams();
+                layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            }
+        };
     }
 }
